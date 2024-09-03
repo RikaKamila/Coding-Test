934c381ad64667bbaa620d27d3c0b9ab73c9c8f5
Rika Kamila <rikakamila1717@gmail.com>
2024-09-03 20:56:53 +0700

    commit terakhir

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let nilai = [8, 3, 9, 2, 5];
 
 function linearSearch(arr,target) {
+
+if (!Array.isArray(arr)) {
+    return "Input pertama harus berupa array";
+if (typeof target !== 'number') {
+    return "input kedua harus berupa angka";
+    }
+}
     for (let i = 0; i < Array.length; i++)
-        if (arr[i] === target) {
+        if (arr[i] < min) {
             return i;
         }
         return false;
 }
+
+let number = linearSearch ([8, 3, 9, 2, 5]);
 
 console.log(linearSearch(number, 3));
 console.log(linearSearch(number, 1));
